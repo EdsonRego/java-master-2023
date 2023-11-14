51c506a16ee5182665fb3aebf36f4a04f3bb2144
EdsonRego <edsonxrego@gmail.com>
2023-11-14 10:45:44 -0300

    Java Servlet - convertido para JSP

--- webapp-form/src/main/java/com/edsonrego/apiservlet/webapp/form/FormServlet.java
+++ webapp-form/src/main/java/com/edsonrego/apiservlet/webapp/form/FormServlet.java
@@ -14,65 +14,61 @@ import java.util.List;
 
 @WebServlet("/registro")
 public class FormServlet extends HttpServlet {
-
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
-        try (PrintWriter out = resp.getWriter()) {
 
-            String username = req.getParameter("username");
-            String password = req.getParameter("password");
-            String email = req.getParameter("email");
-            String pais = req.getParameter("pais");
-            String[] lenguajes = req.getParameterValues("lenguajes");
-            String[] roles = req.getParameterValues("roles");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String email = req.getParameter("email");
+        String pais = req.getParameter("pais");
+        String[] lenguajes = req.getParameterValues("lenguajes");
+        String[] roles = req.getParameterValues("roles");
 
-            String idioma = req.getParameter("idioma");
-            boolean habilitar = req.getParameter("habilitar") != null &&
-                    req.getParameter("habilitar").equals("on");
-            String secreto = req.getParameter("secreto");
+        String idioma = req.getParameter("idioma");
+        boolean habilitar = req.getParameter("habilitar") != null &&
+                req.getParameter("habilitar").equals("on");
+        String secreto = req.getParameter("secreto");
 
-            List<String> errores = new ArrayList<>();
+        List<String> errores = new ArrayList<>();
 
-            if(username == null || username.isBlank()){
-                    errores.add("el user name es requerido");
-            }
+        if (username == null || username.isBlank()) {
+            errores.add("el user name es requerido");
+        }
 
-            if(password == null || password.isBlank()){
-                errores.add("el password no puede ser vacio");
-            }
+        if (password == null || password.isBlank()) {
+            errores.add("el password no puede ser vacio");
+        }
 
-            if(email == null || !email.contains("@")){
-                errores.add("el email es requerido y debe tener un formato de correo");
-            }
+        if (email == null || !email.contains("@")) {
+            errores.add("el email es requerido y debe tener un formato de correo");
+        }
+        if (pais == null || pais.equals("") || pais.equals(" ")) {
+            errores.add("el pais es requerido");
+        }
 
-            if(pais == null || pais.equals("") || pais.equals(" ")){
-                errores.add("el pais es requerido");
-            }
+        if (lenguajes == null || lenguajes.length == 0) {
+            errores.add("debe seleccionar al menos un tema");
+        }
+        if (roles == null || roles.length == 0) {
+            errores.add("debe seleccionar al menos un role");
+        }
+        if (idioma == null) {
+            errores.add("debes seleccionar un idioma");
+        }
+        if (errores.isEmpty()) {
+            try (PrintWriter out = resp.getWriter()) {
 
-            if(lenguajes == null || lenguajes.length == 0){
-                errores.add("debe seleccionar al menos un tema");
-            }
+                out.println("<!DOCTYPE html>");
+                out.println("<html>");
+                out.println("     <head>");
+                out.println("         <meta charset=\"UTF-8\">");
+                out.println("         <title>Resultado Form</title>");
+                out.println("     </head>");
+                out.println("     <body>");
+                out.println("         <h1>Resultado Form!</h1>");
 
-            if(roles == null || roles.length == 0){
-                errores.add("debe seleccionar al menos un role");
-            }
-
-            if (idioma == null){
-                errores.add("debes seleccionar un idioma");
-            }
-
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("     <head>");
-            out.println("         <meta charset=\"UTF-8\">");
-            out.println("         <title>Resultado Form</title>");
-            out.println("     </head>");
-            out.println("     <body>");
-            out.println("         <h1>Resultado Form!</h1>");
-
-            out.println("         <ul>");
-            if(errores.isEmpty()) {
+                out.println("         <ul>");
                 out.println("           <li>Username: " + username + "</li>");
                 out.println("           <li>Password: " + password + "</li>");
                 out.println("           <li>Email: " + email + "</li>");
@@ -91,15 +87,17 @@ public class FormServlet extends HttpServlet {
                 out.println("            <li>Idioma: " + idioma + "</li>");
                 out.println("            <li>Habilitado: " + habilitar + "</li>");
                 out.println("            <li>Secreto: " + secreto + "</li>");
-            }else {
-                errores.forEach(error ->{
+                out.println("            </ul>");
+                out.println("     </body>");
+                out.println("</html>");
+            }
+        } else {
+                /*errores.forEach(error ->{
                     out.println("<li>" + error + "</li>");
                 });
-                out.println("<p><a href=\"/webapp-form/index.html\">volver</a></p>");
-            }
-            out.println("            </ul>");
-            out.println("     </body>");
-            out.println("</html>");
+                out.println("<p><a href=\"/webapp-form/index.jsp\">volver</a></p>");*/
+            req.setAttribute("errores", errores);
+            getServletContext().getRequestDispatcher("/index.jsp").forward(req, resp);
         }
     }
 }
